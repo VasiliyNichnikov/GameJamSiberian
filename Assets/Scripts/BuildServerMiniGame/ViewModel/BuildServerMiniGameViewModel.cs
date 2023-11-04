6094e5cb3dd1c453f9bd14daf9f9836216dc2897
Max <max.yablokov02@yandex.ru>
2023-11-05 00:21:49 +0300

    Мини-игра. Вылевка на сервере
    3 ModeView, 2 IModelView, 2 View, 1 Config, 2 Prefabs

---
+++ Assets/Scripts/BuildServerMiniGame/ViewModel/BuildServerMiniGameViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using BuildServerMiniGame.View;
+using UnityEngine;
+
+namespace BuildServerMiniGame.ViewModel
+{
+    public class BuildServerMiniGameViewModel : IBuildServerMiniGameViewModel
+    {
+        public IBuildServerMiniGameQuestionsViewModel QuestionsViewModel { get; }
+        public string TaskText { get; }
+        public string HintText { get; }
+        private readonly List<string> _errorsTexts;
+        private readonly string _sussesText;
+
+        public BuildServerMiniGameViewModel(Configs.BuildServerMiniGameData data)
+        {
+            TaskText = data.Task;
+            HintText = data.Hint;
+            QuestionsViewModel = new BuildServerMiniGameQuestionsViewModel(data);
+            _errorsTexts = data.Errors.ToList();
+            _sussesText = data.Susses;
+        }
+
+        public bool CheckAnswer()
+        {
+            var variables = QuestionsViewModel.GetQuestionsStatus();
+            /*
+             * (¬w∧¬x)∧(y∧z)∧¬(t→w)
+             *    t  w  x  y  z
+             *    1  0  0  1  1
+             */
+            return (!variables[1] & !variables[2]) & (variables[3] & variables[4]) & !(!variables[0] | variables[1]);
+        }
+
+        public string GetRandomError()
+        {
+            return _errorsTexts[Random.Range(0, _errorsTexts.Count)];
+        }
+
+        public string GetSusses()
+        {
+            return _sussesText;
+        }
+    }
+}
---
+++ Assets/Scripts/Configs/BuildServerMiniGameData.cs
@@ -0,0 +1,24 @@
+﻿#nullable enable
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Configs
+{
+    [CreateAssetMenu(fileName = "BuildServerMiniGameData", menuName = "Configs/BuildServerMiniGameData", order = 0)]
+    public class BuildServerMiniGameData : ScriptableObject
+    {
+        public IReadOnlyCollection<string> Questions => _questions;
+        public IReadOnlyCollection<string> Errors => _errors;
+        public string Susses => _susses;
+        public string Task => _task;
+        public string Hint => _hint;
+        
+        [SerializeField] private string[] _questions = null!; // t  w  x  y  z
+        [SerializeField] private string[] _errors = null!;
+        [SerializeField] private string _susses = null!;
+        [SerializeField] private string _task = null!;
+        [SerializeField] private string _hint = null!;
+    }
+}
